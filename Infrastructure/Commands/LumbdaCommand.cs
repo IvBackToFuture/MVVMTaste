dd3c136860c92a8ddd7c30d5eaba86a40d90fe73
Иван Сугробов <i.a.sugrobov@mgutm.ru>
2022-04-22 02:32:17 +0300

    Главный класс Команды и легкие правки расположения файлов

---
+++ Infrastructure/Commands/LumbdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MVVMTaste.Infrastructure.Commands.Base;
+
+namespace MVVMTaste.Infrastructure.Commands
+{
+    internal class LumbdaCommand : Command
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+
+        public LumbdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute(parameter);
+    }
+}
