b74bd121e453741c4b4adb2abbd112962332daa5
Иван Сугробов <i.a.sugrobov@mgutm.ru>
2022-04-24 02:30:39 +0300

    Добавление моделей

---
+++ Models/PlaceInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTaste.Models
+{
+    internal class PlaceInfo
+    {
+        public string Name { get; set; }
+        public Point Location { get; set; }
+        public IEnumerable<ComfirmedCount> Counts { get; set; }
+    }
+}
